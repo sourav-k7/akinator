bfe686db1636dc6030d367cc37ba0585d135224b
sourav-k7 <neymarsk01@gmail.com>
2021-04-13 19:46:51 +0530

    fixed bug

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
 
 
-app.get('/home',(req,res)=>{
+app.get('/',(req,res)=>{
   res.render('index');
 })
 app.get('/start',async (req,res)=>{
