d7e8668f0d398c5551535136898875df5bc354ab
sourav-k7 <neymarsk01@gmail.com>
2021-04-15 23:01:22 +0530

    testing

--- app.js
+++ app.js
@@ -23,8 +23,8 @@ app.get('/start',async (req,res)=>{
   aki= new Aki(region,childMode);
  var data= await aki.start();
     // const sess = data[0];
-    console.log(data);
-    // res.cookie('session',sess);
+   
+    res.cookie('session',aki.session);
     // res.cookie('sign',data[1]);
     // res.cookie('addr',data[2]);
     // res.cookie('step',data[3]);
