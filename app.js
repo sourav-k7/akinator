f973d2b06435ea3271d30276bbfeafa641e23b40
sourav-k7 <neymarsk01@gmail.com>
2021-04-15 21:19:22 +0530

    added session and childmode

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const { Aki } = require('aki-api');
 const path = require('path');
 const ejs =require('ejs');
 const engine = require('ejs-mate');
+const cookieParser = require('cookie-parser');
 const region = 'en';
 let aki ;
 const app = express()
+app.use(cookieParser());
 app.use(express.urlencoded({extended:true}));
 app.engine('ejs',engine);
 app.set('view engine','ejs');
@@ -13,24 +15,35 @@ app.set('views',path.join(__dirname,'views'));
 
 
 app.get('/',(req,res)=>{
+  
   res.render('index');
 })
 app.get('/start',async (req,res)=>{
-  aki= new Aki(region);
-  await aki.start()
+  const childMode=true;
+  aki= new Aki(region,childMode);
+ var data= await aki.start();
+    res.cookie('session',data[0]);
+    res.cookie('sign',data[1]);
+    res.cookie('addr',data[2]);
+    res.cookie('step',data[3]);
     var question = aki.question;
     res.render('game',{question});
 })
 app.post('/ans',async (req,res)=>{
   
-  await aki.step(req.body.answer);
+  await aki.step(req.cookies,req.body.answer);
+  res.cookie('step',parseInt(req.cookies.step)+1);
   const question =aki.question;
   if(aki.progress>=90)
   {
-     await aki.win();
-     const guess = aki.answers[0];
-   return  res.render('win',{guess});
-  }
+    try
+    {await aki.win(req.cookies);
+      const guess = aki.answers[0];
+      return  res.render('win',{guess});}
+      catch(error){
+        console.log(error);
+      }
+    }
  return res.render('game',{question});
 })
 const port = process.env.PORT||3000
